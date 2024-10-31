87d292066ed87879a5b70eb5bc5388dd3584b5d4
Evobucket <williamvick2004@gmail.com>
2024-10-31 12:44:54 -0400

    Added root node and made it root of project
    Added a root node that levels will be a child of. Added an inventory node that doesnt do anything yet.  The new root scene is root.tscn.

---
+++ marinara-game/scripts/inventory.cs
@@ -0,0 +1,20 @@
+using Godot;
+using System;
+
+public partial class inventory : Node3D
+{
+	Godot.Collections.Array<ingredient> ingrediantlist;
+	
+	
+	
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+	
+}
