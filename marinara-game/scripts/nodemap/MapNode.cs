c6653b87a6fa4ad579a20a38e1819f5f193c4dee
parker <parker.e.davis@gmail.com>
2024-11-13 21:25:43 -0500

    Nodemap scene instantiates full set of blank nodes

---
+++ marinara-game/scripts/nodemap/MapNode.cs
@@ -0,0 +1,11 @@
+using Godot;
+using System;
+
+public partial class MapNode : Godot.Node
+{
+	public void Build()
+	{
+		//Position.X = x; //= new Vector2(x, y);
+		GD.Print("NODE HAS BEEN BUILT");
+	}
+}
---
+++ marinara-game/scripts/nodemap/NodeMap.cs
@@ -0,0 +1,23 @@
+using Godot;
+using System;
+
+public partial class NodeMap : Node
+{
+	//private TextureButton testNode;
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		for (int i = 0; i < (8*5); i++)
+		{
+			var testNode = (TextureButton) GD.Load<PackedScene>("res://scenes/nodemap/blank_node.tscn").Instantiate();
+			AddChild(testNode);
+		}
+		
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		
+	}
+}
