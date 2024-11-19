9215f6264fba0d91f0055991f3784889ecad9fd8
thealkalineknight <139799163+thealkalineknight@users.noreply.github.com>
2024-11-19 14:06:18 -0500

    Added ingredient randomizer

--- marinara-game/scripts/classes/ingredient.cs
+++ marinara-game/scripts/classes/ingredient.cs
@@ -1,15 +1,25 @@
 using Godot;
 using System;
+using Godot.Collections;
 
 public partial class ingredient : Node
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
+	// list all ingredient options here
+	public Array<ingredient> allIngredients;
+	
+	// returns a random ingredient object from full list of ingredients
+	public ingredient randomIngredient() {
+		int index;
+		ingredient option;
+		Random random = new Random();
+		
+		index = random.Next(0, this.allIngredients.Count);
+		
+		option = this.allIngredients[index];
+		
+		return option;
 	}
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
+	
+	// GETTERS
+	public ingredient getIngredient(int index) {return allIngredients[index];}
 }
