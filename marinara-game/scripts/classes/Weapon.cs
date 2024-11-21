0a94b4b9349da226a76a4dde465ad42e36e9c2d3
Coola101 <ajgignac@icloud.com>
2024-11-21 10:14:38 -0800

    Updated the Weapon.cs Class
    Now has implementation to load PackedScene bullet and Texture2D weaponSprite in the constructor. Small changes to commented code.
    bullet is now a PackedScene and is no longer in a comment.

--- marinara-game/scripts/classes/Weapon.cs
+++ marinara-game/scripts/classes/Weapon.cs
@@ -11,13 +11,14 @@ public partial class Weapon : GodotObject
 	private float downTime; //Downtime after using a weapon before the player can switch or attack again
 	private int projectileCount; //Number of hurtboxes/projectiles spawned by attacking a single time
 
-	//private object bullet; //Scene object with the default projectile fired by this weapon.
+	private PackedScene bullet; //Scene object with the default projectile fired by this weapon.
 	private float damage; //Damage dealt by an individual projectile or swing of this weapon
 	private float lifeTime; //Amount of time before the hurt box for the weapon's attack despawns
 	private float instantVelocity; //Velocity applied to each bullet when created
 	
 	private float healMod; //Modifier that affects the amount of health healed by eating this weapon
 
+	private int useCost; //Number of uses consumed each time the weapon is used.
 	private int maxUses; //Maximum number of times the player can attack with this weapon before it is destroyed
 	private int uses; //Number of times the player can attack with this weapon before it is destroyed
 	
@@ -25,7 +26,7 @@ public partial class Weapon : GodotObject
 	
 	///Constructors/Destructors
 	
-	///<summary>
+	///<summary>	Constructor that reads in all relevant stats for a weapon.
 	///<param name="_name"> string passed in for the weapon's name </param>
 	///<param name="_autoFire"> bool passed in for the weapon's "autoFire" </param>
 	///<param name="_downTime"> float passed in for the weapon's "downTime" </param>
@@ -33,12 +34,13 @@ public partial class Weapon : GodotObject
 	///<param name="_damage"> float passed in for the weapon's damage value </param>
 	///<param name="_lifeTime"> float passed in for the weapon's "lifeTime" </param>
 	///<param name="_instantVelocity"> float passed in for the weapon's "instantVelocity" </param>
+	///<param name="_useCost"> int passed in for the weapon's "useCost" </param>
 	///<param name="_maxUses"> int passed in for the weapon's "maxUses" </param>
 	///<param name="iList[]"> array of ingredients passed in when creating a weapon. These are the Secondary Ingredients used to craft the weapon. </param>
 	///<param name="spritePath"> string representation of file path leading to the texture that will be used for "weaponSprite" </param>
 	///<param name="bulletPath"> string representation of file path leading to the scene that will be used for "bullet" </param>
 	///</summary>
-	Weapon(string _name, bool _autoFire, float _downTime, int _projectileCount, float _damage, float _lifeTime, float _instantVelocity, int _maxUses, /*Ingredient[] iList,*/ string spritePath, string bulletPath) {
+	Weapon(string _name, bool _autoFire, float _downTime, int _projectileCount, float _damage, float _lifeTime, float _instantVelocity, int _useCost, int _maxUses, /*Ingredient[] iList,*/ string spritePath, string bulletPath) {
 		//Instantiate variables
 		name = _name;
 		autoFire = _autoFire;
@@ -47,11 +49,12 @@ public partial class Weapon : GodotObject
 		damage = _damage;
 		lifeTime = _lifeTime;
 		instantVelocity = _instantVelocity;
-		healMod = 0.15f; //0.15 is the default value for all weapons except Pistols // someone forgot an f and it caused an error
+		healMod = 0.15f; //0.15 is the default value for all weapons except Pistols
+		useCost = _useCost;
 		maxUses = _maxUses;
 
-		//Fetch weaponSprite & bullet
-
+		weaponSprite = GD.Load<Texture2D>("res://scenes/weapons/"+spritePath);
+		bullet = GD.Load<PackedScene>("res://sprites//prefabs/weapons/"+bulletPath);
 
 		//Apply Secondary Ingredient modifiers
 		//SUGGESTION: each Secondary Ingredient class/struct should have a "applyStats()" type of function that modifies the weapon's stats.
@@ -76,6 +79,7 @@ public partial class Weapon : GodotObject
 	public float getLifeTime() {return lifeTime;}
 	public float getInstantVelocity() {return instantVelocity;}
 	public float getHealMod() {return healMod;}
+	public int getUseCost() {return useCost;}
 	public int getMaxUses() {return maxUses;}
 	public int getUses() {return uses;}
 	
@@ -84,13 +88,20 @@ public partial class Weapon : GodotObject
 
 	///FUNCTIONS
 	
-	//TO-DO: Heal function
+	///<summary>	Function where player will "eat" part of their weapon in order to heal.
+	///<returns> The amount of health the player should heal from eating the weapon. </returns>
+	///</summary>
 	public float eatWeapon() {
 		expendUse(maxUses/4);
-		return 10*healMod;
+		return 10*healMod; //10 is an arbitrary number. Will be changed as health stats are finalized
 	}
 
-	//TO-DO: Shoot function
+	//TO-DO: Shoot function - Prerequisite: bullet Scene needs to be made properly.
+	/*public void fireWeapon() {
+		expendUse(useCost);
+		Node newBullet = instantiate(bullet);
+		newBullet.Bullet.setStats([INSERT PARAMETERS BEING PASSED]);
+	}*/
 	
 	///<summary>
 	///<param name="value"=1> Number of uses being expended by an action of this weapon
@@ -98,7 +109,7 @@ public partial class Weapon : GodotObject
 	private void expendUse(int value = 1) {
 		uses -= value;
 		//if (uses<=0)
-		//Destroy or otherwise delete this item?
+		//Destroy or otherwise delete this item? Probably will need to call a delete function in a parent class or something?
 	}
 
 }
