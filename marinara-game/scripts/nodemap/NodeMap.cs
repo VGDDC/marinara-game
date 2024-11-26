808afcdd14e3e25ac9642b66a525572d0524cd46
parker davis <theparkerevan>
2024-11-25 23:39:14 -0500

    Different node types

--- marinara-game/scripts/nodemap/NodeMap.cs
+++ marinara-game/scripts/nodemap/NodeMap.cs
@@ -3,22 +3,90 @@ using System;
 
 public partial class NodeMap : Node
 {
+	private string[] RANDOMIZATION_OPTIONS;
+	private RoomNode[,] nodeMap;
 	//private TextureButton testNode;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		for (int i = 0; i < (8*5); i++)
-		{
-			var testNode = (RoomNode) GD.Load<PackedScene>("res://scenes/nodemap/blank_node.tscn").Instantiate();
-			testNode.Build();
-			AddChild(testNode);
-		}
-		
+		// Could probably factor this out into file, but that's not a high priority.
+		RANDOMIZATION_OPTIONS = new string[8]{
+			"EXPLORATION 100;1 100",
+			"EXPLORATION 80 WAVEDEFENSE 20;2 50 3 50",
+			"EXPLORATION 70 WAVEDEFENSE 30;P 1",
+			"EXPLORATION 60 WAVEDEFENSE 30 SHOP 10;P 1",
+			"EXPLORATION 55 WAVEDEFENSE 35 SHOP 10;3 50 4 50",
+			"EXPLORATION 45 WAVEDEFENSE 40 SHOP 15;2 50 3 50",
+			"SHOP 100;2 100",
+			"BOSS 100;1 100"
+		};
+
+		RandomizeNodeMap();
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
 		
+	}
+
+	//========================================================
+	//            N O D E  M A P   M E T H O D S
+	//
+	//                  Proceed with Caution
+	//========================================================
+	public void SetCell(int x, int y, string type)
+	{
+		nodeMap[x,y].OfType(type).Refresh();
+	}
+
+	public void ClearNodeMap()
+	{
+		nodeMap = new RoomNode[8,5];
+		int i = 0;
+
+		for (int row = 0; row < 5; row++)
+		{
+			for (int col = 0; col < 8; col++)
+			{
+				var newNode = (RoomNode) GD.Load<PackedScene>("res://scenes/nodemap/blank_node.tscn").Instantiate();
+				newNode.OfType("empty")
+						.At(col, row)
+						.Refresh();
+				
+				nodeMap[col,row] = newNode;
+				
+				AddChild(newNode);
+				i++;
+			}
+		}
+	}
+
+	//========================================================
+	//       R A N D O M I Z A T I O N   M E T H O D S
+	//
+	//                  Proceed with Caution
+	//========================================================
+	public void RandomizeNodeMap()
+	{
+		ClearNodeMap();
+
+		// First cell is always Exploration
+		SetCell(0, 2, "exploration");
+
+		// Second to last column has 1 shop and 1 exploration
+		if (GD.Randi() % 2 == 1)
+		{
+			SetCell(6, 1, "shop");
+			SetCell(6, 3, "exploration");
+		}
+		else
+		{
+			SetCell(6, 3, "shop");
+			SetCell(6, 1, "exploration");
+		}
+
+		// Last cell is always the boss
+		SetCell(7, 2, "boss");
 	}
 }
--- marinara-game/scripts/nodemap/RoomNode.cs
+++ marinara-game/scripts/nodemap/RoomNode.cs
@@ -1,12 +1,95 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 public partial class RoomNode : TextureButton
 {
-	public void Build()
+	private int x;
+	private int y;
+	private bool exists;
+	private string type;
+
+	private List<RoomNode> connections;
+	
+	// Build
+	public RoomNode OfType(string _type)
 	{
-		TextureNormal = "res://sprites/map/map_node_white.png";
-		//Position.X = x; //= new Vector2(x, y);
-		GD.Print("NODE HAS BEEN BUILT");
+		this.type = _type;
+		return this;
+	}
+
+	public RoomNode At(int _x, int _y)
+	{
+		this.x = _x;
+		this.y = _y;
+
+		Position = new Vector2(x*80+20, y*80);
+
+		return this;
+	}
+	
+	public void Refresh()
+	{
+		TextureNormal = (Texture2D) GD.Load("res://sprites/map/map_node_" + type + ".png");
+		connections = new List<RoomNode>();
+
+		// Empty nodes do not "exist" and will not be rendered
+		if (type.Equals("empty"))
+		{
+			exists = false;
+			Visible = false;
+		}
+		else
+		{
+			exists = true;
+			Visible = true;
+		}
+	}
+
+	// Setters
+	public void AddConnection(RoomNode node)
+	{
+		connections.Add(node);
+
+		
+	}
+
+	// Getters
+	public Vector2 GetPos()
+	{
+		return Position;
+	}
+	
+	public float GetX()
+	{
+		return Position[0];
+	}
+	
+	public float GetY()
+	{
+		return Position[1];
+	}
+	
+	public string GetNodeType()
+	{
+		return this.type;
+	}
+
+	public bool Exists()
+	{
+		return this.exists;
+	}
+
+	// Setters
+	public void SetType(string _type)
+	{
+		this.type = _type;
+	}
+
+	
+	// Dunder Zone
+	public override string ToString()
+	{
+		return this.type + " (" + this.x + ", " + this.y + ")";
 	}
 }
