7ecbf2b0c774ffc9ab0b655219f44668cc070147
parker davis <theparkerevan>
2025-03-20 13:29:20 -0400

    begining work on food_truck interior

---
+++ marinara-game/scripts/control_utils/teleporter.cs
@@ -0,0 +1,18 @@
+using Godot;
+using System;
+
+public partial class teleporter : Node3D
+{
+	private Area3D collisionArea;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		//collisionArea = GD.Get
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
